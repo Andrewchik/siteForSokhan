d01c4b6b0af677ea5af216a52a98f8d8d6a6af6b
Andrewchik <andriy.goryachiy16@gmail.com>
2020-12-13 23:11:55 +0200

    fifth commit

---
+++ js/sendInstagram.js
@@ -0,0 +1,30 @@
+// https://api.telegram.org/bot1425750862:AAEVpcJ4seOtCzWcnHkdHBgTa8aLaRSEZ3c/getUpdates
+
+// https://api.telegram.org/bot1425750862:AAEVpcJ4seOtCzWcnHkdHBgTa8aLaRSEZ3c/sendMessage?chat_id=382712023&text=hi_i_am_a_bot
+
+// https://api.telegram.org/bot1425750862:AAEVpcJ4seOtCzWcnHkdHBgTa8aLaRSEZ3c/getUpdates
+
+
+
+document.querySelector('#sendMessage').onclick = function() {
+  
+    let message = document.querySelector('#addMessage').value
+    let shablonMessage = 'Повідомлення з сайту:                                                                                           '
+    const token = '1425750862:AAEVpcJ4seOtCzWcnHkdHBgTa8aLaRSEZ3c'
+    chat_id='-1001168723591'
+    // chat_id='382712023'
+    let url = 'https://api.telegram.org/bot'+ token +'/sendMessage?chat_id='+ chat_id +'&text='+ shablonMessage +''
+    let xhttp = new XMLHttpRequest()
+    xhttp.open("GET", url+message, true)
+    xhttp.send()
+    document.querySelector('#addMessage').value = ''
+    alert('Повідомлення відправлено успішно!')
+    
+}
+
+
+  
+    
+    
+
+
